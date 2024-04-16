1e58901faf520875009aa8865ea053595c4143cd
JUGGUM <gayoung801@naver.com>
2024-04-17 08:35:38 +0900

    Feat : ExampleController.java (stream)

--- src/main/java/hello/springmvc/basic/practice/ExampleController.java
+++ src/main/java/hello/springmvc/basic/practice/ExampleController.java
@@ -4,6 +4,7 @@ package hello.springmvc.basic.practice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +14,9 @@ import java.util.List;
 public class ExampleController {
 
     public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("apple", "banana", "orange");
+        list.forEach(System.out::println);
 
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve");
         // 문자열 길이가 5 이상인 이름만 필터링
@@ -38,5 +42,19 @@ public class ExampleController {
         for(int number : numberArray){
             System.out.println(number);
         }
+
+        enum Day {
+            MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+        }
+
+        // java 14부터 사용하는 간결과 switch
+        Day day = Day.MONDAY;
+        int value = switch (day) {
+            case MONDAY, FRIDAY, SUNDAY -> 6;
+            case TUESDAY                -> 7;
+            case THURSDAY, SATURDAY     -> 8;
+            case WEDNESDAY              -> 9;
+            default                     -> throw new IllegalStateException("Invalid day: " + day);
+        };
     }
 }
