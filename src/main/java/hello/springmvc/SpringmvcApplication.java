83bad328ba1f3e2e9c05eb8c55224ab0ebe284ee
JUGGUM <gayoung801@naver.com>
2024-05-10 13:59:15 +0900

    chore : @TransactionEventListener를 사용하기위한실습

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -1,9 +1,13 @@
 package hello.springmvc;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // 스케줄러 기능 활성화
+@EnableBatchProcessing // 배치 기능 활성화
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
