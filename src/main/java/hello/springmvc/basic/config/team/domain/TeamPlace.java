2b4b20302169bc493b66b3e51238c1dfe49d566f
JUGGUM <gayoung801@naver.com>
2024-05-29 14:32:03 +0900

    Feat : TeamPlace.java

---
+++ src/main/java/hello/springmvc/basic/config/team/domain/TeamPlace.java
@@ -0,0 +1,47 @@
+package hello.springmvc.basic.config.team.domain;
+
+import hello.springmvc.basic.config.team.BaseEntity;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamPlace extends BaseEntity {
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+/*    public static final String UNKNOWN_TEAM_PLACE_NAME = "알수없음";
+    public static final TeamPlace UNKNOWN_TEAM_PLACE = new TeamPlace(
+            new Name(UNKNOWN_TEAM_PLACE_NAME));
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Name name;
+
+    public TeamPlace(final Name name) {
+        this.name = name;
+    }
+
+    public String getNameValue() {
+        return name.getValue();
+    }*/
+}
