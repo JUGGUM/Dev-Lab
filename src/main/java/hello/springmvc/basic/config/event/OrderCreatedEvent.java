7f2ba84ebc8b6dba29f3a4e297e14d3505ac9147
JUGGUM <gayoung801@naver.com>
2024-06-20 08:36:57 +0900

    Feat : OrderCreatedEvent.java

---
+++ src/main/java/hello/springmvc/basic/config/event/OrderCreatedEvent.java
@@ -0,0 +1,17 @@
+package hello.springmvc.basic.config.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * 이벤트가 동작하는 방법의 예시
+ */
+@Getter
+public class OrderCreatedEvent extends ApplicationEvent {
+
+    private final Long orderId;
+    public OrderCreatedEvent(Object source, Long orderId) {
+        super(source);
+        this.orderId = orderId;
+    }
+}
