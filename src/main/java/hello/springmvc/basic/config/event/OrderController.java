4e76905ebfc39587200885974862020228bbc023
JUGGUM <gayoung801@naver.com>
2024-07-04 09:56:44 +0900

    Feat : OrderController.java

---
+++ src/main/java/hello/springmvc/basic/config/event/OrderController.java
@@ -0,0 +1,14 @@
+package hello.springmvc.basic.config.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+
+    public static void main(String[] args) {
+        OrderService orderService = null;
+    }
+}
