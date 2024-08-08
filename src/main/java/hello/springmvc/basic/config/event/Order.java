bef7e20f56f1f386486a5ce4f785df396354571d
JUGGUM <gayoung801@naver.com>
2024-08-08 09:37:21 +0900

    Feat : Order.java

---
+++ src/main/java/hello/springmvc/basic/config/event/Order.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic.config.event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders")
+@RequiredArgsConstructor
+public class Order {
+
+    @Id
+    private Long id;
+
+    private OrderStatus status;
+
+}
