de0f7ed245dc8c5376e73819738056a8291c9073
JUGGUM <gayoung801@naver.com>
2024-08-09 10:21:08 +0900

    Feat : ScheduleEvent.java

---
+++ src/main/java/hello/springmvc/basic/config/team/ScheduleEvent.java
@@ -0,0 +1,21 @@
+package hello.springmvc.basic.config.team;
+
+
+import lombok.Getter;
+
+@Getter
+public abstract class ScheduleEvent implements DomainEvent<Long> {
+
+    private final Long scheduleId;
+    private final Long teamPlaceId;
+
+    protected ScheduleEvent(final Long scheduleId, final Long teamPlaceId) {
+        this.scheduleId = scheduleId;
+        this.teamPlaceId = teamPlaceId;
+    }
+
+    @Override
+    public Long getDomainId() {
+        return scheduleId;
+    }
+}
