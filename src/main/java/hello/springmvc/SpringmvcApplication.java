08eed6e9cca8280926441b7fcba731d8276cc62d
JUGGUM <gayoung801@naver.com>
2024-10-21 15:47:14 +0900

    Update : @ConfigurationPropertiesScan 어노테이션 스캔

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -3,11 +3,13 @@ package hello.springmvc;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling // 스케줄러 기능 활성화
 @EnableBatchProcessing // 배치 기능 활성화
+@ConfigurationPropertiesScan // class path 존재하는 모든 ConfigurationProperties Scan(필수❗️)
 public class SpringmvcApplication {
 
 	public static void main(String[] args) {
