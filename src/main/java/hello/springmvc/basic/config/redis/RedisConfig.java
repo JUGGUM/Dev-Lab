34a701b28c5287e6fd09d661607bb03aec691552
JUGGUM <gayoung801@naver.com>
2024-10-25 15:50:31 +0900

    Update : RedisConfig.java

--- src/main/java/hello/springmvc/basic/config/redis/RedisConfig.java
+++ src/main/java/hello/springmvc/basic/config/redis/RedisConfig.java
@@ -9,21 +9,23 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableRedisHttpSession // 스프링 세션을 Redis에 저장하도록 설정
 @EnableRedisRepositories // REDIS를 사용한다고 명시
 public class RedisConfig {
-
     private final RedisProperties redisProperties;
 
     // RedisProperties로 yaml에 저장한 host, post를 연결
+    // Lettuce 를 이용해서 Redis에 접속
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
 
-    // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
+    // serializer(직렬화) 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
