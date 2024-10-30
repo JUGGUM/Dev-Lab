4220430e06d6e22675ab7c06f8f9f7a1767baa27
JUGGUM <gayoung801@naver.com>
2024-10-30 10:22:25 +0900

    Feat : ScheduleUpdateRequest

---
+++ src/main/java/hello/springmvc/basic/domain/team/request/ScheduleUpdateRequest.java
@@ -0,0 +1,23 @@
+package hello.springmvc.basic.domain.team.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+public record ScheduleUpdateRequest(
+
+        String title,
+        String description,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        LocalDateTime startDateTime,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        LocalDateTime endDateTime) {
+
+    public ScheduleUpdateRequest(
+            final String title,
+            final LocalDateTime startDateTime,
+            final LocalDateTime endDateTime) {
+        this(title, null, startDateTime, endDateTime);
+    }
+}
