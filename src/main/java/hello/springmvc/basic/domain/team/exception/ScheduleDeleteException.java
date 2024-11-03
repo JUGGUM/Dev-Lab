53f0963edeb1328740146373c6a218936d87e917
JUGGUM <gayoung801@naver.com>
2024-11-04 08:21:21 +0900

    Feat : ScheduleDeleteException.java

---
+++ src/main/java/hello/springmvc/basic/domain/team/exception/ScheduleDeleteException.java
@@ -0,0 +1,8 @@
+package hello.springmvc.basic.domain.team.exception;
+
+public class ScheduleDeleteException extends CustomNotFoundException{
+    public ScheduleDeleteException(final Long scheduleId) {
+        super(String.format("조회한 일정이 존재하지 않습니다. - request info { schedule_id : %d }", scheduleId));
+    }
+
+}
