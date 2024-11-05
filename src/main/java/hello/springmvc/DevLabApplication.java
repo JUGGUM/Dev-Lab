391fec98adf5cd75fb11ec996c01c7055ae94139
JUGGUM <gayoung801@naver.com>
2024-11-05 16:25:55 +0900

    Update : DevLabApplication.java

--- src/main/java/hello/springmvc/DevLabApplication.java
+++ src/main/java/hello/springmvc/DevLabApplication.java
@@ -4,9 +4,11 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableScheduling // 스케줄러 기능 활성화
 @EnableBatchProcessing // 배치 기능 활성화
 @ConfigurationPropertiesScan // class path 존재하는 모든 ConfigurationProperties Scan(필수❗️)
